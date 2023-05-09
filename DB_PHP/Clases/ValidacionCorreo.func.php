f17e0d35d4ea52ee2e4456ed5f719ac259648b3a
Betoniix <miguelhumberto522@gmail.com>
2023-05-09 14:12:46 -0600

    Validacion añadida

--- DB_PHP/Clases/ValidacionCorreo.func.php
+++ DB_PHP/Clases/ValidacionCorreo.func.php
@@ -1,5 +1,4 @@
 <?php
-
 function CorreoValidacion(String $correo){
     $patron = '/^(?=.{1,64}@)(?=.{5,}@((gmail\.com)|(outlook\.com)|(yahoo\.com)|(icloud\.com)|(aol\.com))$)[A-Za-z0-9._%+-]+@((gmail\.com)|(outlook\.com)|(yahoo\.com)|(icloud\.com)|(aol\.com))$/';
 
